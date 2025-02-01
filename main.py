3512fc0096a82894132e3a61bab65cd51ff5b1f1
Bharti Bansal <68323304+bharti0219@users.noreply.github.com>
2025-01-31 20:45:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+print("hi", 7)
+print(6)  #print is a function
+#commenting
+# print("biee \n bye")
+# print(17*13)
+'''
+comment 
+multi line
+code
+option+down arrow
+'''
+"""
+also
+a 
+MULTILINE
+comment 
+"""
+
+print("I\'m a 'good girl' \nviewers are also \"good\" girls/boys")
+
+#multiple values in print
+print("hi", 4, 5, 6)
+
+#sep>> separates
+print(4, 5, 6, sep=",")
+print(4, 5, 6, sep=",", end="end\n")  #default sep is space and end is \n
+print(3, 4, 5)  # taking default vale of end and sep
+print("bharti")
